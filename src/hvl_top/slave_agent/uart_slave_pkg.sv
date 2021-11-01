4ae1fcd7f9df27a58cdeab422e3fee4aa712d705
anithasamanthula <anithasamanthula@mirafra.com>
2021-11-02 00:03:14 +0530

    added scoreboard and coverage

--- src/hvl_top/slave_agent/uart_slave_pkg.sv
+++ src/hvl_top/slave_agent/uart_slave_pkg.sv
@@ -20,6 +20,7 @@ package uart_slave_pkg;
   // Include all other files
   //-------------------------------------------------------
   `include "slave_tx.sv"
+  `include "uart_slave_seq_item_converter.sv"
   `include "slave_agent_config.sv"
   `include "slave_sequencer.sv"
   `include "slave_driver_proxy.sv"
