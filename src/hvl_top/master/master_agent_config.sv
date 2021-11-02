514a79aae4e4724826b435b46cbced1f2d6bfa2b
jyoshnalakshmi <jyoshnalakshmi@mirafra.com>
2021-11-02 22:39:16 +0530

    added master files

--- src/hvl_top/master/master_agent_config.sv
+++ src/hvl_top/master/master_agent_config.sv
@@ -10,7 +10,8 @@ class master_agent_config extends uvm_object;
 
   // Variable: is_active
   // Used for creating the agent in either passive or active mode
-  uvm_active_passive_enum is_active=UVM_ACTIVE;  
+  uvm_active_passive_enum is_active=UVM_ACTIVE; 
+
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
@@ -22,13 +23,12 @@ endclass : master_agent_config
 // Construct: new
 //
 // Parameters:
-//  name - master_agent_config
-//  parent - parent under which this component is created
+// name - master_agent_config
+// parent - parent under which this component is created
 //--------------------------------------------------------------------------------------------
 function master_agent_config::new(string name = "master_agent_config");
   super.new(name);
 endfunction : new
-
 
 `endif
 
